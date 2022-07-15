887acc82d094ff1105ebd6765853fa6afdea3d67
Jhon Ballesteros <Jhvarga@gap.com>
2022-07-15 11:25:47 -0500

    calendar every 30 minutes

--- src/app/shared/components/meeting-calendar/meeting-calendar.component.ts
+++ src/app/shared/components/meeting-calendar/meeting-calendar.component.ts
@@ -6,7 +6,8 @@ import { SalaService } from 'src/app/private/http/sala.service';
 import { ISala } from 'src/app/_models/sala.interface';
 
 
-const HOURS: number[] = [8, 9, 10, 11, 13, 14, 15, 16, 17, 18]
+const HOURS: number[] = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+const MINUTES: number[] = [0, 30]
 
 @Component({
   selector: 'app-meeting-calendar',
@@ -15,11 +16,11 @@ const HOURS: number[] = [8, 9, 10, 11, 13, 14, 15, 16, 17, 18]
 })
 export class MeetingCalendarComponent implements OnInit {
   @Input() default: boolean = false;
-  @Output() selectedSala= new EventEmitter<ISala>;
-  @Input() inputSala:ISala|null=null;
+  @Output() selectedSala = new EventEmitter<ISala>;
+  @Input() inputSala: ISala | null = null;
   selected: Date | null;
   minDate!: Date;
-  dateFilter = (date: Date): boolean => {return true;}
+  dateFilter = (date: Date): boolean => { return true; }
 
   // listDates: Array<Date>;
   currentListDate: ({ isActive: boolean, sala: ISala } | undefined)[] = [];
@@ -28,7 +29,7 @@ export class MeetingCalendarComponent implements OnInit {
 
   constructor(
     private salaService: SalaService,
-    private snackBar : MatSnackBar,
+    private snackBar: MatSnackBar,
   ) {
     this.selected = null;
     this.selectedSala.emit(undefined);
@@ -39,8 +40,8 @@ export class MeetingCalendarComponent implements OnInit {
   onDateChanged(dateChanged: Date | null): void {
 
     if (this.default) {
-      if(dateChanged===null){
-        dateChanged=new Date();
+      if (dateChanged === null) {
+        dateChanged = new Date();
       }
       this.currentListDate = this.generateSalas(dateChanged);
     }
@@ -55,28 +56,32 @@ export class MeetingCalendarComponent implements OnInit {
     }
   }
 
-  
+
   onSelectDate(d: ({ isActive: boolean, sala: ISala } | undefined)): void {
     this.clearSelectedItems();
     d!.isActive = true;
     this.selectedSala.emit(d?.sala);
   }
 
-  generateSalas(d:Date): { isActive: boolean, sala: ISala }[] {
+  generateSalas(d: Date): { isActive: boolean, sala: ISala }[] {
     const listSalas: { isActive: boolean, sala: ISala }[] = [];
     for (let index = 0; index < HOURS.length; index++) {
-      const element = HOURS[index];
-      const date = new Date(d.getFullYear(),d.getMonth(),d.getDate(),element,d.getMinutes(),0,0 );
-      let newSala: ISala = {
-        id: 0,
-        fecha: date,
-        cupo: 0,
-        link: '',
-        usuarioId: '',
-        nombreUsuario:'',
-        totalCupo:0
+      for (let y = 0; y < MINUTES.length; y++) {
+        const hour = HOURS[index];
+        const minute = MINUTES[y];
+        const date = new Date(d.getFullYear(), d.getMonth(), d.getDate(), hour, minute, 0, 0);
+        let newSala: ISala = {
+          id: 0,
+          fecha: date,
+          cupo: 0,
+          link: '',
+          usuarioId: '',
+          nombreUsuario: '',
+          totalCupo: 0
+        }
+        listSalas.push({ "isActive": false, "sala": newSala });
+
       }
-      listSalas.push({ "isActive": false, "sala": newSala });
     }
     return listSalas;
   }
@@ -94,33 +99,33 @@ export class MeetingCalendarComponent implements OnInit {
     this.salaService.getSalas().subscribe({
       next: (data: ISala[]) => {
         this.listSalas = data;
-        this.dateFilter=(date:Date):boolean=>{
+        this.dateFilter = (date: Date): boolean => {
           return this.listSalas.some(x => x.fecha.toString().slice(0, 10) === date.toISOString().slice(0, 10));
         }
       },
-      error: (err: any) => { 
+      error: (err: any) => {
         this.snackBar.open('Ocurrio un error consultando las salas , comuniquese con el administrador.')
       }
     });
   }
 
   ngOnInit(): void {
-    if(this.default){
+    if (this.default) {
       this.minDate = new Date();
       this.minDate.setDate(this.minDate.getDate() + 1);
     }
-    else{
+    else {
       this.minDate = new Date();
     }
 
-    if(this.inputSala!==null && this.inputSala!==undefined){
+    if (this.inputSala !== null && this.inputSala !== undefined) {
       //Modo edicion
       this.onDateChanged(this.inputSala.fecha);
-      const element = 
-      this.currentListDate
-      .find(x=>x?.sala.fecha.toString().slice(16,21)===this.inputSala?.fecha.toString().slice(16,21));
+      const element =
+        this.currentListDate
+          .find(x => x?.sala.fecha.toString().slice(16, 21) === this.inputSala?.fecha.toString().slice(16, 21));
       this.onSelectDate(element);
-      this.selected=this.inputSala.fecha;
+      this.selected = this.inputSala.fecha;
     }
     if (!this.default) {
       this.loadSalas();
--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    API_URL:"http://localhost:5153/"
+    // API_URL:"http://localhost:5153/"
+    API_URL:"https://infraccion.devaia.com/"
   };
   

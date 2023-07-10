f8338118408c9dedd368253cd919a84f11f80348
Andres Silva <acsm2411>
2023-07-10 08:44:50 -0500

    Revert API_URL to .NET url

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-    API_URL:"http://localhost:3000/"
-    // API_URL:"https://infraccion.devaia.com/"
+    API_URL:"https://infraccion.devaia.com/"
   };
   
